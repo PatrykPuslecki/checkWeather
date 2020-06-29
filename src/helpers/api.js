b0d4835f0de6f71dc088096b301a1b399254b598
PusleckiPatryk <p.puslecki@webmetric.pl>
2020-06-29 10:40:25 +0200

    Deploy script + https on api

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,5 +1,5 @@
 export const API_URL =
-  "http://api.openweathermap.org/data/2.5/weather?units=metric";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric";
 export const API_HOURLY_URL =
-  "http://api.openweathermap.org/data/2.5/forecast?units=metric";
+  "https://api.openweathermap.org/data/2.5/forecast?units=metric";
 export const API_KEY = "&APPID=882cdd78c8b93028565e817022096bcf";
